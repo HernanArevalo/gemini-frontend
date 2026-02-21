7d2bf9f1c3062849b87805c4050e1f18ee967588
Hernán Arévalo <hernanarevalo16@gmail.com>
2026-02-21 20:07:14 -0300

    Fix typo in parameter name for getBasicPrompt function

--- actions/gemini/basic-prompt.action.ts
+++ actions/gemini/basic-prompt.action.ts
@@ -1,15 +1,13 @@
 import geminiApi from "../gemini.api"
 
 
-export const getBasicPrompt = async (promt: string): Promise<string> => {
+export const getBasicPrompt = async (prompt: string): Promise<string> => {
 
     const response = await geminiApi.post('/basic-prompt', {
       prompt,
     },
     { responseType: 'text'}
 );
-
-console.log({data: response.data})
     
 return response.data.trim()
 }
